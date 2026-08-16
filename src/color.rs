b1199dcf283dbb332c9e50dddfcd4a8ce7e2a4fa
Kian Ahmadi <otyyjhct@gmail.com>
2026-08-16 16:05:44 +0330

    added a sphare to be rendered

--- src/color.rs
+++ src/color.rs
@@ -1,9 +1,27 @@
-use crate::{ray::Ray, vec3::RGB};
+use crate::{
+    ray::Ray,
+    vec3::{Point3, RGB},
+};
 use std::io::{Result, Write};
 
+// --- Sphere hit test ---
+pub fn hit_sphere(center: RGB, radius: f64, r: &Ray) -> bool {
+    let oc = center - r.origin();
+    let a = r.direction().dot(r.direction());
+    let b = -2.0 * r.direction().dot(oc);
+    let c = oc.dot(oc) - radius.powi(2);
+
+    b * b - 4.0 * a * c >= 0.0
+}
+
 pub fn ray_color(r: &Ray) -> RGB {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return RGB::new(0.0, 0.0, 1.0);
+    }
+
     let unit_direction = r.direction().unit();
     let a = 0.5 * (unit_direction.y() + 1.0);
+
     (1.0 - a) * RGB::new(1.0, 1.0, 1.0) + a * RGB::new(0.5, 0.7, 1.0)
 }
 
