d1c914a5eec51cb3692f7839487a7b88dedbdfb6
Kian Ahmadi <otyyjhct@gmail.com>
2026-08-25 16:16:51 +0330

    implemented the gradiant though normalizeing the srufcie for shading

--- src/color.rs
+++ src/color.rs
@@ -4,23 +4,29 @@ use crate::{
 };
 use std::io::{Result, Write};
 
-// --- Sphere hit test ---
-pub fn hit_sphere(center: RGB, radius: f64, r: &Ray) -> bool {
+pub fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> Option<f64> {
     let oc = center - r.origin();
     let a = r.direction().dot(r.direction());
     let b = -2.0 * r.direction().dot(oc);
     let c = oc.dot(oc) - radius.powi(2);
+    let discriminant = b * b - 4.0 * a * c;
 
-    b * b - 4.0 * a * c >= 0.0
+    match discriminant {
+        d if d < 0.0 => None,
+        _ => Some((-b - discriminant.sqrt()) / (2.0 * a)),
+    }
 }
 
 pub fn ray_color(r: &Ray) -> RGB {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return RGB::new(0.0, 0.0, 1.0);
+    // Compute surface normal at hit point and map to color
+    if let Some(t) = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        let n = (r.at(t) - Point3::new(0.0, 0.0, -1.0)).unit();
+
+        return 0.5 * RGB::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
 
-    let unit_direction = r.direction().unit();
-    let a = 0.5 * (unit_direction.y() + 1.0);
+    // Compute backround color
+    let a = 0.5 * (r.direction().unit().y() + 1.0);
 
     (1.0 - a) * RGB::new(1.0, 1.0, 1.0) + a * RGB::new(0.5, 0.7, 1.0)
 }
